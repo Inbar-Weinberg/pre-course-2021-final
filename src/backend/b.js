c2368ac858ff6b06cd21b8c316e312c2e534b778
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-02-17 19:07:51 +0200

    completed get task by id

--- src/backend/b.js
+++ src/backend/b.js
@@ -1,11 +1,19 @@
 let tasks = [
   {
+    id: 1,
     text: "An example to-do",
     priority: 1,
     date: 1611662776177
   },
   {
+    id: 2,
     text: "A second example to-do",
+    priority: 4,
+    date: 1611661776177
+  },
+  {
+    id: 3,
+    text: "A third example to-do",
     priority: 4,
     date: 1611661776177
   }
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-const PORT=5000;
+const PORT = 5000;
 const tasks = require("./b");
 
-app.get("/", (req, res) => {
-    res.json(tasks);      
+// get all tasks
+app.get("/b", (req, res) => {
+    res.json(tasks);
+});
+
+// get single task by ID
+app.get("/b/:id", (req, res) => {
+    let compareId = tasks => tasks.id === parseInt(req.params.id);
+    const foundId = tasks.some(compareId);
+    if (foundId)
+        res.json(tasks.filter(compareId));
+    else
+        res.status(400).json({ msg: `No tasks with the ID of ${req.params.id}` });
+});
+// create and add a new task
+app.post("/b", (req, res) => {
+    res.send(red.body);
 });
 
 
